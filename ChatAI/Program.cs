66eaa2770a40bc47fdecc4f485f35ab74a54a49f
prajkta.patil <prajkta.patil@KELLGGNLPTP1615>
2023-07-17 17:44:13 +0530

    Add project files.

---
+++ ChatAI/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    const string API_KEY = "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";
+    static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+
+     string prompts=   Console.ReadLine();
+        await Generat(prompts);
+    }
+
+    static async Task Generat(string prompts)
+    {
+       
+            var options = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new
+                    {
+                        role = "user",
+                        content = prompts
+                    }
+                },
+                max_tokens = 3500,
+                temperature = 0.2
+            };
+
+            var json = System.Text.Json.JsonSerializer.Serialize(options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", API_KEY);
+
+            try
+            {
+                var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+            dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+            string result = jsonResponse.choices[0].message.content;
+            Console.WriteLine("Chat Bot: " + result);
+
+
+        }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        
+    }
+
+  
+}
---
+++ ChatBotMvc/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using ChatBotMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace ChatBotMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string API_KEY = "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";
+        private readonly ILogger<HomeController> _logger;
+        static readonly HttpClient client = new HttpClient();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> Get(string input)
+         {
+
+            var promptSections = new[]
+    {
+        "Prompt section 1",
+        "Prompt section 2",
+        // Add more prompt sections as needed
+    };
+
+            var messages = promptSections.Select(section => new
+            {
+                role = "user",
+                content = section
+            }).ToList();
+
+            messages.Add(new
+            {
+                role = "user",
+                content = input
+            });
+
+
+            var options = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new
+                    {
+                        role = "user",
+                        content = input
+                    }
+                },
+                max_tokens = 3500,
+                temperature = 0.2
+            };
+
+            var json = System.Text.Json.JsonSerializer.Serialize(options);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", API_KEY);
+
+            try
+            {
+                var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+                string result = jsonResponse.choices[0].message.content;
+
+                string filePath = "C:\\Users\\prajkta.patil\\source\\repos\\ChatAI\\ChatBotMvc\\Controllers\\hobbie.txt";  // Specify the file path
+                                                                                                                           // Save the input prompt and result in the file
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Input: {input}");
+
+                sb.AppendLine($"Result: {result}");
+
+                System.IO.File.WriteAllText(filePath, sb.ToString());
+
+                return Json(result);
+
+
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message);
+            }
+            return View();
+
+        }
+    }
+}
